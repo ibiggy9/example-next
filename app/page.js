192f03b005076db694b9403f13c994789e75f678
Ian Bigford <ibigford9@gmail.com>
2024-09-25 21:16:57 -0400

    Removed some funky globals that were causing weird behaviour and fixed the footer

--- app/page.js
+++ app/page.js
@@ -2,24 +2,43 @@ import Link from 'next/link'
 
 export default function Home() {
   return (
-    <div>
-      <div className="text-2xl text-black font-bold text-center mt-20">Hello, Welcome to my Personal Website</div>
+    <div className="flex flex-col min-h-screen">
+      <main className="flex-grow mx-auto px-0">
+        <div className="text-2xl text-black font-bold text-center mt-20">
+          Hello, Welcome to my Personal Website
+        </div>
 
+        <div className="mt-4 text-center">
+          <Link
+            href="/portfolio"
+            className="inline-block mt-2 px-4 py-1 text-white bg-blue-500 rounded hover:bg-blue-700 transition"
+          >
+            Check out my Portfolio
+          </Link>
+        </div>
 
-      <div className="mt-4 text-center">
-        <Link href="/portfolio" className="inline-block mt-2 px-4 py-1 text-white bg-blue-500 rounded hover:bg-blue-700 transition">Check out my Portfolio</Link>
-      </div>
+        <div className="mt-4 text-center">
+          <Link
+            href="/blog"
+            className="inline-block mt-2 px-4 py-1 text-white bg-blue-500 rounded hover:bg-blue-700 transition"
+          >
+            Check out my Blog
+          </Link>
+        </div>
+      </main>
 
-      <div className="mt-4 text-center">
-        <Link href="/blog" className="inline-block mt-2 px-4 py-1 text-white bg-blue-500 rounded hover:bg-blue-700 transition">Check out my Blog</Link>
-      </div>
+      <footer className="bg-gray-800 text-white py-8 w-full">
+        <div className="ml-8">
+          To Contact me, please click below:
+        </div>
 
-      <div className="mt-60 ml-8 text-black"> To Contact me, please click below </div>
-
-      <Link href="/contact" className="inline-block mt-4 ml-8 px-4 py-1 text-white bg-blue-500 rounded hover:bg-blue-700 transition">
-        Contact Me
-      </Link>
+        <Link
+          href="/contact"
+          className="inline-block mt-4 ml-8 px-4 py-1 text-white bg-blue-500 rounded hover:bg-blue-700 transition"
+        >
+          Contact Me
+        </Link>
+      </footer>
     </div>
-
   );
 }
